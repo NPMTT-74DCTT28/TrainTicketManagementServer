087c3d45ded6b9913cb3f70ce17b41c1db95542b
Hoang Quoc Phuong <qphwng2005@gmail.com>
2026-04-05 23:35:33 +0700

    Initial commit

---
+++ src/main/java/com/nmptt/ticketapi/TrainTicketManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.nmptt.ticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nmptt/ticketapi/TrainTicketManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmptt.ticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
